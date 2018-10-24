d900d59bdabe0350e71dccc2dfc6666362fa4310
SDiserens <sam.diserens@gmail.com>
2018-10-24 16:18:22 +0100

    main outline

--- Framework/Framework.cpp
+++ Framework/Framework.cpp
@@ -6,6 +6,59 @@
 
 int main()
 {
+	// ----------------------------
+	// ------ Initialisation ------
+	// ----------------------------
+	// Read config file
+
+	// Initialise population
+
+	// Load Modules
+
+		// Validate Modules
+
+	// Load Environment Parameters
+
+	// --------------------------
+	// --- Evolve Environment ---
+	// --------------------------
+	// While timeSimulation < timeEnd
+		// Propagate Object Orbits
+
+		// Determine Events
+			// Collision Detection
+
+				// Log
+
+			// Generate Explosions
+
+				// Log
+
+			// Check for Pre-specified Events
+
+				// Log
+
+		// Update population
+			// Simulate Fragmentations
+
+				// Log
+
+			// Generate Launches
+
+				// Log
+
+			// Remove Decayed Objects
+
+				// Log
+
+
+	// ----------------------------
+	// ------ End Simulation ------
+	// ----------------------------
+	// Save final population
+
+	// Write Logs to output files
+
     return 0;
 }
 
