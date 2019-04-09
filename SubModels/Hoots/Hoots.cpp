605eb31dc50411e0c4e9f8cc34841464de74dc0a
SDiserens <sam.diserens@gmail.com>
2019-04-09 17:14:42 +0100

    hoots wrapper + SGP4

---
+++ SubModels/Hoots/Hoots.cpp
@@ -0,0 +1,12 @@
+// Hoots.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "Modules/Collision_Algorithms/HootsFilter.h"
+#include <json\json.h>
+
+int main()
+{
+    return 0;
+}
+
