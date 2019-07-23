99c90ae7495df4ff7688009542b2a15c547bec43
SDiserens <sam.diserens@gmail.com>
2019-07-23 17:27:25 +0100

    SGP4

---
+++ SubModels/SGP4Test/SGP4Test.cpp
@@ -0,0 +1,36 @@
+// SGP4Test.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "Modules\Propagators\SGP4\SGP4wrap.h"
+
+
+int main()
+{
+	// Open scenario file
+	// Read scenario file into 
+	// close file
+
+	
+	// For each Scenario
+		// Create Population
+
+		// Read TLE and generate objects
+			// For set in file Read 3 lines and create object
+
+			// Add to Population
+
+		//Set Start time
+
+		// While time < endTime
+			// Propagate in specified timestep
+		
+			// Store State vector 
+
+
+	// Create Output file
+		// Write state vectors for each scenario at each propagation step
+
+    return 0;
+}
+
