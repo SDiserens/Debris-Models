7a9374247a5e339e916a3b4366d2f023f1c44539
SDiserens <sam.diserens@gmail.com>
2020-07-02 18:55:59 +0100

    Hoots gpu work and debugging

---
+++ x64/Release/Iridium2006_Comparison.cmd
@@ -0,0 +1,9 @@
+::Test
+Framework.exe -c "Cube" -t 1 -f "Collisions_Iridium2006.json"
+Framework.exe -c "Cube" -t 2 -f "Collisions_Iridium2006.json"
+Framework.exe -c "Cube" -t 5 -f "Collisions_Iridium2006.json"
+Framework.exe -c "Cube" -t 10 -f "Collisions_Iridium2006.json"
+Framework.exe -c "Cube" -t 20 -f "Collisions_Iridium2006.json"
+Framework.exe -c "Cube" -t 50 -f "Collisions_Iridium2006.json"
+
+Framework.exe -c "OrbitTrace" -t 0.0 -mc 1 -f "Collisions_Iridium2006.json"
